eb7267af28adf2d7c31da6bae3c0ea05c286fabe
ryota <>
2020-08-08 12:27:48 +0900

    記事を表示する

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.create({title: '素晴らしい記事です', content: 'これはすごい記事です。素晴らしい'})
+Article.create({title: '2つ目の記事です', content: 'そこそこの記事です。'})

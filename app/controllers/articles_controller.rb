6664ad2abd801eae7388c9641c4811576b49a981
ryota <>
2020-08-08 18:34:01 +0900

    新規投稿フォームの作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,14 @@
 class ArticlesController < ApplicationController
     def index   #一覧表示に使うメソッド
-        @articles = Article.all
+      @articles = Article.all
     end
 
     def show
-        @article = Article.find(params[:id])
+      @article = Article.find(params[:id])
     end
     
     def new
-      
+      @article = Article.new  
     end
 
 end

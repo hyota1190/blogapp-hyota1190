4d17afa8e24dbd0ab8ce7c56b40ce4a5e6f56c90
ryota <>
2020-08-12 22:47:40 +0900

    自分の投稿のみ編集する機能（リファクタリング）

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :articles, dependent: :destroy  #複数形にする、ユーザーが削除されたら、articleも削除される
+  
+  def has_written?(article)
+    self.articles.exists?(id: article.id)
+  end
 end

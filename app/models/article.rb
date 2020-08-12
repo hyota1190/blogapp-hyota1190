b8ff0b6830dac49ac06a0cf634309eae6a0ba681
ryota <>
2020-08-12 23:33:52 +0900

    ユーザが存在することを前提にしてコード修正

--- app/models/article.rb
+++ app/models/article.rb
@@ -26,6 +26,10 @@ class Article < ApplicationRecord
   def display_created_at
     I18n.l(self.created_at, format: :default)
   end
+  
+  def author_name
+    user.display_name
+  end
 
   private
   def validate_title_and_content_length

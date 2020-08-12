0fa8ef4499c76548cf047702413577747b098b8b
ryota <>
2020-08-12 11:29:21 +0900

    エラーメッセージの表示

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,22 @@ class ArticlesController < ApplicationController
     end
     
     def new
-      @article = Article.new  
+      @article = Article.new 
+    end
+    
+    def create
+      @article = Article.new(article_params)
+      if @article.save
+        redirect_to article_path(@article)
+      else
+        render :new
+      end      
     end
 
+    private
+    
+    def article_params  #privateにする
+      params.require(:article).permit(:title, :content)
+      # params{article: { title: "aaa", content:"bbb"}}
+    end
 end

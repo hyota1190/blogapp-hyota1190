dc0ddb01bc4d945b36dc49fc37f55f42888e4a6f
ryota <>
2020-08-12 20:16:11 +0900

    add custom validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -12,8 +12,20 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum: 2, maximum: 100 },
                     format: { with: /\A(?!\@)/ }
   validates :content, presence: true, length: { minimum: 10}, uniqueness: true
+  
+  validate :validate_title_and_content_length
+  #validate 独自ルール
 
   def display_created_at
     I18n.l(self.created_at, format: :default)
+  end
+  
+  private
+  def validate_title_and_content_length
+    char_count = self.title.length + self.content.length
+    unless char_count > 100
+      errors.add(:content,'100文字以上で！')
+      #自分でエラーを追加するときの構文
+    end
   end
 end

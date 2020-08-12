0ef3246bba2d874ae5fefc8edad685317490294c
ryota <>
2020-08-12 23:29:41 +0900

    DBのスキーマを修正

--- db/migrate/20200803135522_create_articles.rb
+++ db/migrate/20200803135522_create_articles.rb
@@ -1,8 +1,9 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
-      t.string :title
-      t.text :content
+      t.references :user, null: false
+      t.string :title, null: false
+      t.text :content, null: false  #validationはActive Record側の制限なので、DB側の制限として加えた方が尚良い
       t.timestamps
     end
   end
